257981f48b443207f24e3b4632a0438b87da8a69
liangjinyong <liangjy881@gmail.com>
2023-06-08 23:59:53 +0900

    ch11 accessing mysql using php: connect to mysql

---
+++ public/ch11_accessing_mysql_using_php/01_querying_mysql_with_php.php
@@ -0,0 +1,13 @@
+<?php
+// # Querying a MySQL Database with PHP
+echo '<a href="index.html">Index</a><br><br>';
+
+// # The Process
+/*
+1. Connect to MySQL and select the database to use.
+2. Prepare a query string.
+3. Perform the query.
+4. Retrieve the results and output them to a web page.
+5. Repeat steps 2 to 4 until all desired data has been retrieved.
+6. Disconnect from MySQL.
+*/
---
+++ public/ch11_accessing_mysql_using_php/03_connecting_to_mysql.php
@@ -0,0 +1,25 @@
+<?php
+// # Connecting to a MySQL Database
+require_once '02_login.php';
+
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
+// # Building and executing a query
+$query = "SELECT * FROM classics";
+$result = $pdo->query($query);
+print_r($result);
+
+// # Fetching a result
+while ($row = $result->fetch()) {
+    echo 'Author:   ' . htmlspecialchars($row['author']) . "<br>";
+    echo 'Title:    ' . htmlspecialchars($row['title']) . "<br>";
+    echo 'Category: ' . htmlspecialchars($row['category']) . "<br>";
+    echo 'Year:     ' . htmlspecialchars($row['year']) . "<br>";
+    echo 'ISBN:     ' . htmlspecialchars($row['isbn']) . "<br><br>";
+}
+
+?>
