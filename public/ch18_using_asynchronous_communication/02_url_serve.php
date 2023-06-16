9c5ff728857e256313a81e45579978d53aaca22f
liangjinyong <liangjy881@gmail.com>
2023-06-16 17:30:33 +0900

    ch22 finished

--- public/ch18_using_asynchronous_communication/02_url_serve.php
+++ public/ch18_using_asynchronous_communication/02_url_serve.php
@@ -1,11 +1,9 @@
 <?php
-/*
 // Post
 if (isset($_POST['url']))
 {
     echo file_get_contents('http://' . sanitizeString($_POST['url']));
 }
-*/
 
 // Get
 if (isset($_GET['url']))
@@ -13,7 +11,7 @@ if (isset($_GET['url']))
     echo file_get_contents("http://" . sanitizeString($_GET['url']));
 }
 
-function SanitizeString($var)
+function sanitizeString($var)
 {
     $var = strip_tags($var);
     $var = htmlentities($var);
